f29922fb1c07941c34cf118ce18494cfc81a8e45
sai1781 <96867450+sai1781@users.noreply.github.com>
2022-09-15 16:50:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Nav from "./Components/Nav";
+import Main from "./Components/Main";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
